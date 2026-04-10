18b2cf3c74ff8dbb75cbf4a34bd79ed93bb6e1e7
Alex Jonker <alex.jonker2008@gmail.com>
2026-04-10 13:02:27 +0200

    Test

--- start.py
+++ start.py
@@ -3,7 +3,12 @@ import mpv
 video1 = "/videos/video1.mp4"
 video2 = "/videos/video2.mp4"
 
-player = mpv.MPV(input_default_bindings=True, input_vo_keyboard=True)
+player = mpv.MPV(
+    input_default_bindings=True,
+    input_terminal=True,
+    terminal=True,
+    vo="tct",
+)
 
 player.pause = True
 
